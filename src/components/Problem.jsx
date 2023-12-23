f17e42848c677cc4ac6eaefec9ba9a863632c8cd
Tsaha11 <tuhin727066@gmail.com>
2023-12-23 21:03:00 +0530

    Interface Ready

---
+++ src/components/Problem.jsx
@@ -0,0 +1,32 @@
+import "./Problem.css"
+const Problem=()=>{
+    return <>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted easy">Card subtitle</h6>
+            <div className="tags">
+              <div className="tag">
+                <span>DP</span>
+              </div>
+              <div className="tag">
+                <span>String</span>
+              </div>
+              <div className="tag">
+                <span>Array</span>
+              </div>
+              <div className="tag">
+                <span>Bits</span>
+              </div>
+            </div>
+            <a href="#" class="card-link">
+              Card link
+            </a>
+            <a href="#" class="card-link">
+              Another link
+            </a>
+          </div>
+        </div>
+    </>
+}
+export default Problem;
